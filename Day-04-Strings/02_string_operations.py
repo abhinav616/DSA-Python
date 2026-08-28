6c4685f923920cf775ce6040fe3281252198c61a
abhinav616 <abhinav.arasu.s@gmail.com>
2026-08-28 23:45:20 +0530

    Create 02_string_operations.py

---
+++ Day-04-Strings/02_string_operations.py
@@ -0,0 +1,15 @@
+s = "  Hello Bro  "
+
+print(s.lower())
+print(s.upper())
+print(s.strip())
+
+words = "I love DSA".split()
+print(words)
+
+result = " ".join(words)
+print(result)
+
+print("Hello" + " " + "Bro")
+print("Hello"[1:4])
+print("Hello"[::-1])
