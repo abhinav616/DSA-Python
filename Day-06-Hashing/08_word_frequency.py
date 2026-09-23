c6fb8ff21ca03cd45dd187a54a8a1f14251ccf8d
abhinav616 <abhinav.arasu.s@gmail.com>
2026-09-23 20:34:16 +0530

    Create 08_word_frequency.py

---
+++ Day-06-Hashing/08_word_frequency.py
@@ -0,0 +1,13 @@
+text = "cat dog cat bird dog cat"
+
+words = text.split()
+
+frequency = {}
+
+for word in words:
+    if word in frequency:
+        frequency[word] += 1
+    else:
+        frequency[word] = 1
+
+print(frequency)
