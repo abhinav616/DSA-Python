14d3a862f36bae46260e0f41a6874008215ab1fc
abhinav616 <abhinav.arasu.s@gmail.com>
2026-09-23 20:32:59 +0530

    Create 03_element_frequency.py

---
+++ Day-06-Hashing/03_element_frequency.py
@@ -0,0 +1,11 @@
+arr = [1, 2, 2, 3, 1, 2, 4]
+
+frequency = {}
+
+for i in range(len(arr)):
+    if arr[i] in frequency:
+        frequency[arr[i]] += 1
+    else:
+        frequency[arr[i]] = 1
+
+print(frequency)
